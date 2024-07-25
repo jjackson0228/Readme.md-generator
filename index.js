7867f0c89dba39cfb40df875714d21113230517a
James Jackson <jjackson0228@gmail.com>
2024-07-25 00:13:35 -0400

    finished the basic to get the file running and make a basic readme

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -62,7 +63,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    const readmeContent = generateREADME(answers);
+    const readmeContent = generateMarkdown(answers);
     writeToFile("README.md", readmeContent);
   });
 }
